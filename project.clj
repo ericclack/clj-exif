632e08bd9ca632b749d856e93e74edc61e0065e3
Eric Clack <eric@bn7.net>
2023-07-01 16:31:34 +0100

    Switch to 1.0-alpha3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.commons/commons-imaging "1.0-SNAPSHOT"]]
+                 [org.apache.commons/commons-imaging "1.0-alpha3"]]
   :repositories [
                  ["apache.snapshots" "http://repository.apache.org/snapshots"]])
 
